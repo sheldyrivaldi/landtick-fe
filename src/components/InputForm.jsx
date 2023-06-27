6127500ab982611b20457c19debb3c768482de32
sheldyrivaldi <sheldyrivaldi@gmal.com>
2023-06-28 01:00:09 +0700

    first commit

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,10 @@
+import "./../assets/css/index.css";
+const InputForm = (props) => {
+  return (
+    <div>
+      <input type="text" className=" w- text-2xl border-2 border-soft p-2 placeholder-soft" placeholder="Username" />
+    </div>
+  );
+};
+
+export default InputForm;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        avenir: ["Avenir", "sans-serif"],
+      },
+      colors: {
+        soft: "#B1B1B1",
+        "soft-pink": "#EC7AB7",
+        "strong-pink": "#EC7A7A",
+      },
+    },
+  },
+  plugins: [],
+};
